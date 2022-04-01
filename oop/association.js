709be5e8825b95b451a11cda12407feeda5df403
Serge Chvorun <elifnull@protonmail.com>
2022-03-31 23:36:59 -0400

    OOP practice

--- oop/association.js
+++ oop/association.js
@@ -1,3 +1,25 @@
+class BankAccount {
+    constructor(intRate, balance) { // don't forget to add some default values for these parameters!
+    	// your code here! (remember, this is where we specify the attributes for our class)
+	// don't worry about user info here; we'll involve the User class soon
+    }
+    deposit(amount) {
+	// your code here
+    }
+    withdraw(amount) {
+	// your code here
+    }
+    displayAccountInfo() {
+	// your code here
+    }
+    yieldInterest() {
+	// your code here
+    }
+}
+
+
+
+
 class User {
     constructor(name, email) {
         this.name = name;
@@ -5,3 +27,8 @@ class User {
         this.account = new BankAccount(intRate=0.02, balance=0);	// this is the NEW line
     }
 }
+
+const account1 = new BankAccount(intRate=0.02, balance=100);
+const account2 = new BankAccount(balance=100, intRate=0.02);
+
+
