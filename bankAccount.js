63c3844fc1af16b37e72b9d3f791e71e6c3b890c
Serge Chvorun <elifnull@protonmail.com>
2022-04-04 23:01:41 -0400

    class bankaccount

---
+++ bankAccount.js
@@ -0,0 +1,19 @@
+class BankAccount {
+    constructor(intRate, balance) { // don't forget to add some default values for these parameters!
+    	// your code here! (remember, this is where we specify the attributes for our class)
+	// don't worry about user info here; we'll involve the User class soon
+    }
+    deposit(amount) {
+	// your code here
+    }
+    withdraw(amount) {
+	// your code here
+    }
+    displayAccountInfo() {
+	// your code here
+    }
+    yieldInterest() {
+	// your code here
+    }
+}
+    
